4c521eab47fdc939a82b7e5416a66a8af13767a5
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2023-11-17 17:28:17 -0500

    Time: 112 ms (66.28%), Space: 24.5 MB (69.85%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,6 +2,54 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> tripletResult;
+        int target = 0;
+        sort(nums.begin(),nums.end());
+
+        for(int i = 0; i < nums.size(); i++){
+
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.size() - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == target){
+                    tripletResult.push_back({nums[i], nums[j], nums[k]});
+                
+
+                    while(j < k && nums[j] == nums[j+1]){
+                        j++;
+                    }
+
+                    while(j < k && nums[k] == nums[k-1]){
+                        k--;
+                    }
+
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return tripletResult;
+
+    }
+};
+
+
+/*
+
+Using a set 
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> tripletResult;
         set<vector<int>> tripletSet;
         int target = 0;
 
@@ -34,3 +82,5 @@ public:
 
     }
 };
+
+*/
