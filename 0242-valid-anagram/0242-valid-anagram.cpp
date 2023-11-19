78897095fdb0c920b0403e5f705b8f14c206f00e
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2023-11-18 22:03:41 -0500

    Time: 11 ms (45.10%), Space: 7.8 MB (45.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,44 +2,10 @@
 using namespace std;
 class Solution {
 public:
-    // 2 map solution - O(n) time complexity
+    // O(n log n) - becuase of the sorting time
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
-            return false;
-        }
-
-
-        map<char, int> sMap;
-        map<char, int> tMap;
-
-        for(char letter : s){
-            sMap[letter]++;
-        }
-
-        for(char letter : t){
-            tMap[letter]++;
-        }
-
-        for(const auto& entry : sMap){
-            char letter = entry.first;
-            int frequencySVect = entry.second;
-
-            if(tMap.find(letter) == tMap.end() || tMap[letter] != frequencySVect) {
-                return false;
-            }
-        }
-        
-        for(const auto& entry : tMap){
-            char letter = entry.first;
-            int frequencyTVect = entry.second;
-
-            if(sMap.find(letter) == sMap.end() || sMap[letter] != frequencyTVect){
-                return false;
-            }
-        }
-
-        return true;
-
-
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s == t;
     }
 };
